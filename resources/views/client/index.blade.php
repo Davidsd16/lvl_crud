4f2f1d1be35d38493a7511810720fce9e62c1ef7
david <davidsanchezdiaz72@gmail.com>
2023-12-04 20:11:08 +0100

    Conectado Vistas y Controlador (Implementando ROUTES)

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,24 @@
+@extends('theme.base');
+
+@section('content')
+    <div class="container py-5 text-center">
+        <h1>Listado de Clientes</h1>
+        <a href="{{ route('client.index') }}" class="btn btn-primary"> Crear Cliente</a>
+
+        <table class="table mx-auto table-striped">
+            <thead>
+                <th>Nombre</th>
+                <th>Saldo</th>
+                <th>Acciones</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>FelinoHost</td>
+                    <td>0.0</td>
+                    <td>Eliminar - Editar</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+@endsection
